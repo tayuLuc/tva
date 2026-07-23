b749c887d7376199082f57949957cca0079054ca
user <>
2026-07-23 17:43:09 +0300

    fmt: cargo fmt --all

--- crates/tva-core/src/adapters/dssim.rs
+++ crates/tva-core/src/adapters/dssim.rs
@@ -13,23 +13,13 @@ impl FrameComparator for DssimComparator {
         let w = a.width() as usize;
         let h = a.height() as usize;
 
-        let pixels_a: Vec<RGB8> = a
-            .as_bytes()
-            .chunks_exact(3)
-            .map(|c| RGB8::new(c[0], c[1], c[2]))
-            .collect();
-        let pixels_b: Vec<RGB8> = b
-            .as_bytes()
-            .chunks_exact(3)
-            .map(|c| RGB8::new(c[0], c[1], c[2]))
-            .collect();
+        let pixels_a: Vec<RGB8> = a.as_bytes().chunks_exact(3).map(|c| RGB8::new(c[0], c[1], c[2])).collect();
+        let pixels_b: Vec<RGB8> = b.as_bytes().chunks_exact(3).map(|c| RGB8::new(c[0], c[1], c[2])).collect();
 
-        let img_a = ctx
-            .create_image_rgb(&pixels_a, w, h)
-            .ok_or(TvaError::CompareFailed("dssim create_image_a".into()))?;
-        let img_b = ctx
-            .create_image_rgb(&pixels_b, w, h)
-            .ok_or(TvaError::CompareFailed("dssim create_image_b".into()))?;
+        let img_a =
+            ctx.create_image_rgb(&pixels_a, w, h).ok_or(TvaError::CompareFailed("dssim create_image_a".into()))?;
+        let img_b =
+            ctx.create_image_rgb(&pixels_b, w, h).ok_or(TvaError::CompareFailed("dssim create_image_b".into()))?;
 
         let (val, _) = ctx.compare(&img_a, &img_b);
         Ok(val.into())
